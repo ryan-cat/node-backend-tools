6bb553423578676313bd231bb567431d776fa3e8
= <rmc86@pitt.edu>
2020-07-30 18:54:11 -0400

    undo failing test

--- src/errors/errors.test.ts
+++ src/errors/errors.test.ts
@@ -22,7 +22,7 @@ describe('errors', () => {
     expect(isInstance(new AuthorizationError(''))).toBeTruthy();
     expect(isInstance(new AuthorizationActionError(''))).toBeTruthy();
     expect(isInstance(new APIError(''))).toBeTruthy();
-    expect(isInstance(new InternalServerError(''))).toBeFalsy();
+    expect(isInstance(new InternalServerError(''))).toBeTruthy();
   });
 
   it('should have correct message for user input error', () => {
