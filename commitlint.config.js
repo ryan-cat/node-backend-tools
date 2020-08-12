e9a7b1454929ecb8af73d3f256d0ac116283e48f
= <ryancat.dev@gmail.com>
2020-08-11 23:49:12 -0400

    fix: updated release process

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,1 +1,8 @@
-module.exports = {extends: ['@commitlint/config-conventional']};
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [2, 'always', 500],
+    'body-max-line-length': [2, 'always', 500],
+    'footer-max-line-length': [2, 'always', 500]
+  }
+};
