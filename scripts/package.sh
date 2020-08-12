79fd4efbee4db7ba9d4a1ab7d2c94314302aaa47
= <ryancat.dev@gmail.com>
2020-08-12 00:14:07 -0400

    build: modified build process to output to dist

--- scripts/package.sh
+++ scripts/package.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 
-rm -rf build
 rm -rf dist
 yarn build
-mkdir -p build
-cp -r ./dist/* ./build
-cd build
+cd dist
 find . -name '*.test.js' -delete
 find . -name '*.test.d.ts' -delete
 cd ..
-rm -rf dist
