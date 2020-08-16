88e6b88648851a6d5bc956b17fae85bdb9a85427
= <ryancat.dev@gmail.com>
2020-08-16 17:43:05 -0400

    build: fixed commitlint to work with semantic release

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,8 +1,8 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional']
-  // rules: {
-  //   'header-max-length': [2, 'always', 500],
-  //   'body-max-line-length': [2, 'always', 500],
-  //   'footer-max-line-length': [2, 'always', 500]
-  // }
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'header-max-length': [2, 'always', 500],
+    'body-max-line-length': [2, 'always', 500],
+    'footer-max-line-length': [2, 'always', 500]
+  }
 };
